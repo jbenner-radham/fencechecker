b416dec2008257ae65ed1d1f1710aeeefb3b7a30
James Benner <james.benner@gmail.com>
2025-06-04 22:10:03 -0400

    Make runnable as fencechecker script

--- src/fencechecker/__init__.py
+++ src/fencechecker/__init__.py
@@ -4,34 +4,42 @@ from pprint import pprint
 from rich.console import Console, Group
 from rich.panel import Panel
 from rich.syntax import Syntax
-
 from mrkdwn_analysis import MarkdownAnalyzer
 
-analyzer = MarkdownAnalyzer("./TEST.md")
-code_blocks = analyzer.identify_code_blocks().get("Code block")
-py_code_blocks = [code_block for code_block in code_blocks if code_block.get("language") == "python" or "python3"]
 
-completed_processes = [subprocess.run(["python3", "-c", code_block.get("content")], capture_output=True) for code_block in py_code_blocks]
-
-console = Console()
-
-for index, process in enumerate(completed_processes):
-    code_block = py_code_blocks[index]
-    syntax = Syntax(code_block.get("content"), "python")
-
-    if process.returncode == 0:
-        group = Group(
-            f"[bold green]Success[/bold green] [italic](at line: {code_block.get('start_line')})",
-            Panel(syntax),
+def main() -> None:
+    analyzer = MarkdownAnalyzer("./TEST.md")
+    code_blocks = analyzer.identify_code_blocks().get("Code block")
+    py_code_blocks = [
+        code_block
+        for code_block in code_blocks
+        if code_block.get("language") == "python" or "python3"
+    ]
+    completed_processes = [
+        subprocess.run(
+            ["python3", "-c", code_block.get("content")], capture_output=True
         )
-        console.print(Panel(group))
-    else:
-        group = Group(
-            f"[bold red]Error[/bold red] [italic](at line: {code_block.get('start_line')})",
-            Panel(syntax),
-        )
-        console.print(Panel(group))
+        for code_block in py_code_blocks
+    ]
+    console = Console()
 
-error_count = sum(1 for process in completed_processes if process.returncode != 0)
+    for index, process in enumerate(completed_processes):
+        code_block = py_code_blocks[index]
+        syntax = Syntax(code_block.get("content"), "python")
 
-sys.exit(error_count)
+        if process.returncode == 0:
+            group = Group(
+                f"[bold green]Success[/bold green] [italic](at line: {code_block.get('start_line')})",
+                Panel(syntax),
+            )
+            console.print(Panel(group))
+        else:
+            group = Group(
+                f"[bold red]Error[/bold red] [italic](at line: {code_block.get('start_line')})",
+                Panel(syntax),
+            )
+            console.print(Panel(group))
+
+    error_count = sum(1 for process in completed_processes if process.returncode != 0)
+
+    sys.exit(error_count)
